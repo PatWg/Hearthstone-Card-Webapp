b794ff52763c81478ac593094e25df52ef041712
Patrick Wang <patrick.wang@isep.fr>
2020-11-02 00:02:12 +0100

    Changed to Postgresql

--- routes/cards.js
+++ routes/cards.js
@@ -1,43 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "hearthstone"
+const { Pool, Client } = require("pg");
+const pool = new Pool({
+  user: "patrick",
+  host: "localhost",
+  database: "hearthstone",
+});
+const client = new Client({
+  user: "patrick",
+  host: "localhost",
+  database: "hearthstone",
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+client.connect();
 
 /**
  * @GET method: Gets all cards in the database
  */
-router.get('/', function(req, res, next) {
-    getCards(function (error, cardResult) {
-        var results = cardResult;
-        var result = "{\"cards\": {";
-        for (i = 0; i < results.length - 1; i++) {
-            result += "\"" + i + "\":" + JSON.stringify(results[i]) + ",";
-        }
-        result += "\"" + i + "\":" + JSON.stringify(results[results.length - 1]) + "}}";
-        console.log(result);
-        res.send(JSON.stringify(cardResult));
-    });
+router.get("/", function (req, res, next) {
+  getCards(function (error, cardResult) {
+    var results = cardResult["rows"];
+    res.send(results);
+  });
 });
 
 /**
  * @POST method: Gets cards depending on the parameters passed to the function
  * Possible parameters: 'playerClass', 'cost', 'search'
  */
-router.post('/', function (req, res, next) {
-    getCardsWithPredicate(req, function(error, cardResult) {
-        res.send({'status':'OK', 'results': cardResult})
-    });
+router.post("/", function (req, res, next) {
+  getCardsWithPredicate(req, function (error, cardResult) {
+    var results = cardResult["rows"];
+    res.send(results);
+  });
 });
 
 module.exports = router;
@@ -47,11 +42,10 @@ module.exports = router;
  * @param callback
  */
 function getCards(callback) {
-    con.query('SELECT * FROM Card', function (error, rows) {
-        callback(error, rows);
-    });
+  client.query("SELECT * FROM Card", (err, res) => {
+    callback(err, res);
+  });
 }
-
 
 /**
  * Function called when a POST request is received.
@@ -61,48 +55,55 @@ function getCards(callback) {
  * @param callback
  */
 function getCardsWithPredicate(req, callback) {
-    let i;
-    let hasPredicate = false;
-    let sql = 'SELECT * FROM Card';
-    const predicate = [];
-    if (req.body.playerClass != null) {
-        hasPredicate = true;
-        // 'classes' is an array of length 1+
-        let classes = req.body.playerClass;
-        console.log(classes);
-        if (classes.length === 1) predicate.push('(playerClass = \'' + req.body.playerClass + '\')');
-        else {
-            let stringBuilder = "(playerClass IN (";
-            for (i = 0; i < classes.length - 1; i++) {
-                stringBuilder += '\'' + classes[i] + '\',';
-            }
-            stringBuilder += '\'' + classes[i] + '\'))';
-            predicate.push(stringBuilder);
-        }
+  let i;
+  let hasPredicate = false;
+  let sql = "SELECT * FROM Card";
+  const predicate = [];
+  if (req.body.playerClass != null) {
+    hasPredicate = true;
+    // 'classes' is an array of length 1+
+    let classes = req.body.playerClass;
+    console.log(classes);
+    if (classes.length === 1)
+      predicate.push("(playerClass = '" + req.body.playerClass + "')");
+    else {
+      let stringBuilder = "(playerClass IN (";
+      for (i = 0; i < classes.length - 1; i++) {
+        stringBuilder += "'" + classes[i] + "',";
+      }
+      stringBuilder += "'" + classes[i] + "'))";
+      predicate.push(stringBuilder);
     }
+  }
 
-    if (req.body.cost != null) {
-        hasPredicate = true;
-        if (req.body.cost === '7+')  predicate.push('(cost IN (\'7\',\'8\',\'9\',\'10\',\'11\',\'12\'))');
-        else predicate.push('(cost = \'' + req.body.cost + '\')');
+  if (req.body.cost != null) {
+    hasPredicate = true;
+    if (req.body.cost === "7+")
+      predicate.push("(cost IN ('7','8','9','10','11','12'))");
+    else predicate.push("(cost = '" + req.body.cost + "')");
+  }
 
+  if (req.body.search != null) {
+    hasPredicate = true;
+    predicate.push(
+      "(name LIKE '%" +
+        req.body.search +
+        "%' OR description LIKE '%" +
+        req.body.search +
+        "%')"
+    );
+  }
+
+  if (hasPredicate) {
+    sql += " WHERE ";
+    for (i = 0; i < predicate.length; i++) {
+      if (i === 0) sql += predicate[i];
+      else sql += " AND " + predicate[i];
     }
-
-    if (req.body.search != null) {
-        hasPredicate = true;
-        predicate.push('(name LIKE \'%' + req.body.search + '%\' OR description LIKE \'%' + req.body.search + '%\')')
-    }
-
-    if (hasPredicate) {
-        sql += " WHERE ";
-        for (i = 0; i < predicate.length; i++) {
-            if (i === 0) sql += predicate[i];
-            else sql += " AND " + predicate[i];
-        }
-    }
-    sql += ';';
-    console.log(sql);
-    con.query(sql, function (error, rows) {
-        callback(error, rows);
-    })
+  }
+  sql += ";";
+  console.log(sql);
+  client.query(sql, function (error, rows) {
+    callback(error, rows);
+  });
 }
